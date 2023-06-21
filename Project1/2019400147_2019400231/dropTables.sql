0180925ff0631b98cdf22e111f37749f998f02ae
Begüm Yivli <88006241+begumyivli@users.noreply.github.com>
2023-06-21 13:56:35 +0300

    Add files via upload
    In this project I made a basic movie database system, drawed ER diagrams, created create tables and drop tables files in mySQL. The advanced version of that project is Project3 in that repo.

---
+++ Project1/2019400147_2019400231/dropTables.sql
@@ -0,0 +1,7 @@
+
+DROP TABLE Subscribe, Agreement, Buy, Rate, Located, Next_To, Classify, Play;
+DROP TABLE Directed_Movie;
+DROP TABLE Genre, Theatre, Platform;
+DROP TABLE Movie_Session;
+DROP TABLE Director, Audience;
+DROP TABLE Users, Database_Manager;
